59048315b9719162d39496e7079249862d1e3ff6
claytonNighthawk <nighthawk1029tv@gmail.com>
2016-11-19 18:47:23 -0800

    ready for pi testing

--- flask_main.py
+++ flask_main.py
@@ -23,6 +23,7 @@ from apiclient import discovery
 ###
 # Globals
 ###
+import calculate_free_times as cft
 import CONFIG
 import secrets.admin_secrets  # Per-machine secrets
 import secrets.client_secrets # Per-application secrets
@@ -40,11 +41,11 @@ SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 CLIENT_SECRET_FILE = secrets.admin_secrets.google_key_file  ## You'll need this
 APPLICATION_NAME = 'MeetMe class project'
 
-#############################
+####
 #
 #  Pages (routed from URLs)
 #
-#############################
+####
 
 @app.route("/")
 @app.route("/index")
@@ -68,21 +69,30 @@ def choose():
 
     gcal_service = get_gcal_service(credentials)
     app.logger.debug("Returned from get_gcal_service")
-    flask.g.calendars = list_calendars(gcal_service)
+    flask.g.calendars = cft.list_calendars(gcal_service)
 
     if 'calendar_ids' in flask.session:
-        flask.session['events'] = list_events(gcal_service, flask.session['calendar_ids'])   
-        
+        flask.session['events'] = cft.list_events(gcal_service, flask.session['calendar_ids'], flask.session['end_date'], flask.session['daily_begin_time'], flask.session['daily_end_time'], flask.session['ignoreable_events'])   
+     
+    print("printing events")
+    for event in flask.session['events']:
+        print(event)
+    print()   
     return render_template('index.html')
 
 @app.route('/get_free_times', methods=['POST'])
 def get_free_times():
+    """
+    ideally I would like to not put all the events in the cookie
+    but I do not know how to get them from g since it goes away so quickly
+    and as a value on the button would require a lot of effort to get the button value  
+    back as a list of dicts since it is just a string on return
+    """
     app.logger.debug('Redirecting to show free times page')
-    #events = request.form['get_free_times_button']     #ideally would like to not put all the events in the cookie 
-    events = flask.session['events']                    #but I do not know how to get them from g since it goes away really quickly
-                                                        #and as a value on the button would require a lot of effort to get the list  
-    busy_blocks = get_busy_blocks(events)               #back as a list of dicts
-    flask.g.free_times = calc_free_times(busy_blocks)
+    #events = request.form['get_free_times_button']     
+    events = flask.session['events']                                                                   
+    busy_blocks = cft.get_busy_blocks(events, flask.session["daily_end_time"])               
+    flask.g.free_times = cft.calc_free_times(busy_blocks, flask.session['begin_date'], flask.session['end_date'], flask.session['daily_begin_time'], flask.session['daily_end_time'])
 
     return render_template('free_times.html')
 
@@ -204,7 +214,6 @@ def oauth2callback():
 @app.route('/choose_calendars', methods=['POST'])
 def choose_calendars():
     cal_ids = request.form.getlist("to_read")
-    app.logger.debug(cal_ids)
     flask.session['calendar_ids'] = cal_ids
     return flask.redirect(flask.url_for('choose'))
 
@@ -266,6 +275,7 @@ def init_session_values():
     flask.session["daily_end_time"] = interpret_time("5pm")
     app.logger.debug(flask.session["daily_begin_time"])
     app.logger.debug(flask.session["daily_end_time"])
+    flask.session['ignoreable_events'] = None
 
 def interpret_time(text):
     """
@@ -307,237 +317,17 @@ def interpret_date(text):
             raise
     return as_arrow.isoformat()
 
-def next_day(isotext):
-    """
-    ISO date + 1 day (used in query to Google calendar)
-    """
-    as_arrow = arrow.get(isotext)
-    return as_arrow.replace(days=+1).isoformat()
-
 ####
 #
-#  Functions (NOT pages) that return some information
+#  Functions (NOT pages) that return some information are in calculate_free_times.py
 #
 ####
 
-def list_events(service, calendarIDs):
-    """
-    """
-    app.logger.debug("Entering list_events")
-    now = arrow.utcnow()
-    then = next_day(flask.session['end_date'])
-    results = []
-    for calID in calendarIDs:
-        eventsResult = service.events().list(
-            calendarId=calID, timeMin=now, timeMax=then, singleEvents=True,
-            orderBy='startTime').execute()
-        events = eventsResult.get('items', [])
-
-        for event in events:
-
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            end = event['end'].get('dateTime', event['end'].get('date'))
-            event_id = event['id']
-
-            dateTime_start = arrow.get(start)
-            dateTime_end = arrow.get(end) 
-            summary = event['summary']
-            all_day_event = False
-
-            if 'transparency' in event:
-                app.logger.debug("Event '{}' skipped because it is transparent".format(summary))
-                continue
-
-            if 'ignoreable_events' in flask.session and event_id in flask.session['ignoreable_events']:
-                app.logger.debug("Event '{}' ignored because it was selected as ignorable".format(summary))
-                continue
-
-            if str(dateTime_end.time()) == '00:00:00' and str(dateTime_start.time()) == '00:00:00':
-                app.logger.debug("Event '{}' must be nontransparent all day event, will not skip".format(summary))
-                dateTime_end = dateTime_end.replace(minutes=-1)
-                all_day_event = True
-
-            elif str(dateTime_start.time()) <= flask.session["daily_begin_time"] and str(dateTime_end.time()) >= flask.session['daily_end_time']:
-                app.logger.debug("Event '{}' must be nontransparent really long almost all day event, will not skip".format(summary))
-                all_day_event = True
-
-            elif str(dateTime_end.time()) <= flask.session["daily_begin_time"] or str(dateTime_start.time()) >= flask.session['daily_end_time']:
-                app.logger.debug("Event '{}' at {}-{} skipped, out of time range of {}-{}".format(summary, dateTime_start.time(), dateTime_end.time(), flask.session['daily_begin_time'], flask.session['daily_end_time']))
-                continue
-                
-            results.append(
-                {"dateTime_start": dateTime_start.isoformat(),
-                 "dateTime_end": dateTime_end.isoformat(),
-                 "summary": summary,
-                 "event_id": event_id,
-                 "all_day_event": all_day_event,
-                 })
-
-    return sorted(results, key=event_sort_key)
-
-def event_sort_key(event):
-    return event['dateTime_start']
-
-def normalize_ending_time(endTime, event):
-    #scales back end time if greater than daily end   
-    #makes creating free time blocks easier.
-    daily_end_time = flask.session['daily_end_time'].split(":")                     
-    endTime = endTime.replace(hour=int(daily_end_time[0]), minute=int(daily_end_time[1]))   
-    return endTime.isoformat()
-
-def get_busy_blocks(event_list): 
-    events = []
-    for i in range(len(event_list)):
-        #event_list[i].pop('event_id')
-        starti0 = arrow.get(event_list[i-1]['dateTime_start'])
-        endi0 = arrow.get(event_list[i-1]['dateTime_end'])
-        starti1 = arrow.get(event_list[i]['dateTime_start'])    
-        endi1 = arrow.get(event_list[i]['dateTime_end'])
-        if starti0 <= starti1 and endi0 >= endi1:
-            app.logger.debug("Skipping event '{}' because it overlaps '{}' completely".format(event_list[i]['summary'], event_list[i-1]['summary']))
-            continue
-        events.append(event_list[i])
-
-    # print("printing events that don't overlap completely with others")
-    # for event in events:
-    #     print(event)
-    # print()
-
-    busy_blocks = []
-    for i in range(len(events)-1):
-        endi0 = arrow.get(events[i]['dateTime_end'])
-        starti1 = arrow.get(events[i+1]['dateTime_start'])
-        if endi0 >= starti1:
-            events[i+1]['dateTime_start'] = events[i]['dateTime_start']
-            app.logger.debug("comparing '{0}' to '{1}', extending '{1}' to {2}-{3}\n".format(events[i]['summary'], events[i+1]['summary'], arrow.get(events[i+1]['dateTime_start']).time(), arrow.get(events[i+1]['dateTime_end']).time()))
-        else:
-            if str(endi0.time()) > flask.session['daily_end_time']:
-                events[i]['dateTime_end'] = normalize_ending_time(endi0, events[i])                  
-            app.logger.debug("appending '{}'".format(events[i]['summary']))
-            busy_blocks.append(events[i])
-
-    app.logger.debug("appending '{}'".format(events[-1]['summary']))       
-    busy_blocks.append(events[-1])
-
-    print("appended busy blocks")
-    for event in busy_blocks:
-        print(event)
-    print()
-
-    return busy_blocks
-
-def calc_free_times(busy_blocks):
-    begin_date = arrow.get(flask.session['begin_date'])
-    end_date = arrow.get(flask.session['end_date'])
-    days = (end_date-begin_date).days
-
-    daily_begin_time = flask.session['daily_begin_time'].split(":")
-    daily_end_time = flask.session['daily_end_time'].split(":")
-
-    day_dateTime_begin = begin_date.replace(hour=int(daily_begin_time[0]), minute=int(daily_begin_time[1]))
-    day_dateTime_end = begin_date.replace(hour=int(daily_end_time[0]), minute=int(daily_end_time[1]))
-
-    free_times = []
-    for i in range(days+1):
-        free_times.append(
-            {"dateTime_start": day_dateTime_begin.isoformat(),
-             "dateTime_end": day_dateTime_end.isoformat(),
-             "summary": 'free time',
-             })
-        day_dateTime_begin = day_dateTime_begin.replace(days=+1)
-        day_dateTime_end = day_dateTime_end.replace(days=+1)
-
-
-    for i, busy_time in enumerate(busy_blocks):
-        current_time = busy_time['dateTime_start']
-        for j, free_time in enumerate(free_times):
-            if busy_time['all_day_event'] and busy_time['dateTime_start'] <= free_time['dateTime_start']:
-
-                del free_times[j]
-                break
-            elif busy_time['dateTime_end'] <= free_time['dateTime_start']:
-                app.logger.debug('continuing {} <= {}'.format(busy_time['dateTime_end'], free_time['dateTime_start']))
-                continue
-
-            if busy_time['dateTime_end'] <= free_time['dateTime_end']:
-                part1 = {"dateTime_start": free_time['dateTime_start'],
-                         "dateTime_end": busy_time['dateTime_start'],
-                         "summary": 'free time',
-                        }
-                part2 = {"dateTime_start": busy_time['dateTime_end'],
-                         "dateTime_end": free_time['dateTime_end'],
-                         "summary": 'free time',
-                        }
-                app.logger.debug('deleting \n{} and appending \n{} and \n{}\n'.format(free_times[j], part1, part2))
-                del free_times[j]
-
-                if part1['dateTime_start'] < part1['dateTime_end']: #makes sure parts have positive time span
-                    free_times.append(part1)
-
-                if part2['dateTime_start'] < part2['dateTime_end']: #negative or 0 time span would not make sense
-                    free_times.append(part2)
-                break
-
-        free_times.sort(key=event_sort_key)
-
-    return free_times
-
-   
-def list_calendars(service):
-    """
-    Given a google 'service' object, return a list of
-    calendars.  Each calendar is represented by a dict.
-    The returned list is sorted to have
-    the primary calendar first, and selected (that is, displayed in
-    Google Calendars web app) calendars before unselected calendars.
-    """
-    app.logger.debug("Entering list_calendars")  
-    calendar_list = service.calendarList().list().execute()["items"]
-    result = [ ]
-    for cal in calendar_list:
-        kind = cal["kind"]
-        id = cal["id"]
-        if "description" in cal: 
-            desc = cal["description"]
-        else:
-            desc = "(no description)"
-        summary = cal["summary"]
-        # Optional binary attributes with False as default
-        selected = ("selected" in cal) and cal["selected"]
-        primary = ("primary" in cal) and cal["primary"]
-        
-        result.append(
-            {"kind": kind,
-             "id": id,
-             "summary": summary,
-             "selected": selected,
-             "primary": primary,
-             })
-
-    return sorted(result, key=cal_sort_key)
-
-def cal_sort_key(cal):
-    """
-    Sort key for the list of calendars:  primary calendar first,
-    then other selected calendars, then unselected calendars.
-    (" " sorts before "X", and tuples are compared piecewise)
-    """
-    if cal["selected"]:
-         selected_key = " "
-    else:
-         selected_key = "X"
-    if cal["primary"]:
-         primary_key = " "
-    else:
-         primary_key = "X"
-    return (primary_key, selected_key, cal["summary"])
-
-
-#################
+####
 #
 # Functions used within the templates
 #
-#################
+####
 
 @app.template_filter('fmtdate')
 def format_arrow_date(date):
---
+++ test_calc_ft.py
@@ -0,0 +1,47 @@
+from calculate_free_times import *
+import arrow
+
+def same(real, expected):
+    print(real, expected)
+    return real == expected
+
+session_begin_date = '2016-11-20T00:00:00-08:00' 
+session_end_date = '2016-11-26T00:00:00-08:00'
+session_daily_begin_time = '09:00:00' 
+session_daily_end_time = '17:00:00'
+
+'''
+events test multiple different things at once
+event 0 is a generic event
+event 1 and event 2 test to see if get_busy_blocks with combine them
+event 3 is an event that starts before the daily time window but overlaps that time we care about too so it needs to be included 
+event 4 totally overlaps event 3 and so can be ignored
+event 5 is an event that starts before the daily end time but goes past the end of the daily time window, this should be included 
+event 6 is an all day event that test if program can handle events that begin before the time range and end after the time range 
+'''
+events = [{'dateTime_start': '2016-11-21T09:00:00-08:00', 'all_day_event': False, 'dateTime_end': '2016-11-21T11:00:00-08:00', 'summary': 'busy'},
+          {'dateTime_start': '2016-11-21T12:00:00-08:00', 'all_day_event': False, 'dateTime_end': '2016-11-21T13:00:00-08:00', 'summary': "Events that touch but don't overlap 1"},
+          {'dateTime_start': '2016-11-21T13:00:00-08:00', 'all_day_event': False, 'dateTime_end': '2016-11-21T14:00:00-08:00', 'summary': "Events that touch but don't overlap 2"},
+          {'dateTime_start': '2016-11-23T07:00:00-08:00', 'all_day_event': False, 'dateTime_end': '2016-11-23T11:00:00-08:00', 'summary': 'Include this, partially overlaps span'},
+          {'dateTime_start': '2016-11-23T08:30:00-08:00', 'all_day_event': False, 'dateTime_end': '2016-11-23T09:30:00-08:00', 'summary': 'Include this, overlapping event'},
+          {'dateTime_start': '2016-11-23T16:00:00-08:00', 'all_day_event': False, 'dateTime_end': '2016-11-23T19:00:00-08:00', 'summary': 'Include this, partially overlaps span'},
+          {'dateTime_start': '2016-11-24T00:00:00-08:00', 'all_day_event': True, 'dateTime_end': '2016-11-24T23:59:00-08:00', 'summary': 'All day event'}]
+
+def test_get_busy_blocks():
+    busy_blocks = [{'dateTime_end': '2016-11-21T11:00:00-08:00', 'dateTime_start': '2016-11-21T09:00:00-08:00', 'all_day_event': False, 'summary': 'busy'},
+                   {'dateTime_end': '2016-11-21T14:00:00-08:00', 'dateTime_start': '2016-11-21T12:00:00-08:00', 'all_day_event': False, 'summary': "Events that touch but don't overlap 2"},
+                   {'dateTime_end': '2016-11-23T11:00:00-08:00', 'dateTime_start': '2016-11-23T07:00:00-08:00', 'all_day_event': False, 'summary': 'Include this, partially overlaps span'},
+                   {'dateTime_end': '2016-11-23T17:00:00-08:00', 'dateTime_start': '2016-11-23T16:00:00-08:00', 'all_day_event': False, 'summary': 'Include this, partially overlaps span'},
+                   {'dateTime_end': '2016-11-24T17:00:00-08:00', 'dateTime_start': '2016-11-24T00:00:00-08:00', 'all_day_event': True, 'summary': 'All day event'}]
+    assert same(get_busy_blocks(events, session_daily_end_time), busy_blocks)
+
+def test_calc_free_times():
+    busy_blocks = get_busy_blocks(events, session_daily_end_time)
+    free_times = [{'dateTime_start': '2016-11-20T09:00:00-08:00', 'dateTime_end': '2016-11-20T17:00:00-08:00', 'summary': 'free time'},
+                  {'dateTime_start': '2016-11-21T11:00:00-08:00', 'dateTime_end': '2016-11-21T12:00:00-08:00', 'summary': 'free time'},
+                  {'dateTime_start': '2016-11-21T14:00:00-08:00', 'dateTime_end': '2016-11-21T17:00:00-08:00', 'summary': 'free time'},
+                  {'dateTime_start': '2016-11-22T09:00:00-08:00', 'dateTime_end': '2016-11-22T17:00:00-08:00', 'summary': 'free time'},
+                  {'dateTime_start': '2016-11-23T11:00:00-08:00', 'dateTime_end': '2016-11-23T16:00:00-08:00', 'summary': 'free time'},
+                  {'dateTime_start': '2016-11-25T09:00:00-08:00', 'dateTime_end': '2016-11-25T17:00:00-08:00', 'summary': 'free time'},
+                  {'dateTime_start': '2016-11-26T09:00:00-08:00', 'dateTime_end': '2016-11-26T17:00:00-08:00', 'summary': 'free time'}]
+    assert same(calc_free_times(busy_blocks, session_begin_date, session_end_date, session_daily_begin_time, session_daily_end_time), free_times)
